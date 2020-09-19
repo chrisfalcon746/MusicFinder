7c23ba3f4a92296eba4a965db4255c830672d3ce
Christopher Falcon <chrisfalcon59@hotmail.com>
2020-09-19 14:53:09 -0400

    call API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [apiData, setApiData] = useState([]);
+  useEffect(() => {
+    console.log("is this working?");
+    axios
+      .get(`https://www.theaudiodb.com/api/v1/json/1/search.php?s=coldplay`)
+      .then((response) => {
+        setApiData(response.data.artists);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Music finder</h1>
+      {apiData.map((artists) => {
+        console.log(artists);
+        return <h1>{artists.strArtist}</h1>;
+      })}
     </div>
   );
 }
