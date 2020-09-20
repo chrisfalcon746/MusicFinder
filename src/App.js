0429ac3b3483a712b6daca91b8a99a41594cfe79
Christopher Falcon <chrisfalcon59@hotmail.com>
2020-09-20 17:58:20 -0400

    add a page for artists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import FindArtists from "./Page/FindArtists";
+import Artists from "./Page/Artists";
 function App() {
-  const [apiData, setApiData] = useState([]);
-  useEffect(() => {
-    console.log("is this working?");
-    axios
-      .get(`https://www.theaudiodb.com/api/v1/json/1/search.php?s=coldplay`)
-      .then((response) => {
-        setApiData(response.data.artists);
-      });
-  }, []);
   return (
-    <div>
-      <h1>Music finder</h1>
-      {apiData.map((artists) => {
-        console.log(artists);
-        return <h1>{artists.strArtist}</h1>;
-      })}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={FindArtists} />
+        <Route exact path="/Artist" component={Artists} />
+      </Switch>
+    </Router>
   );
 }
 
